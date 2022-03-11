7050316f32f447e273e97ad262af3469deb0f151
Felipe Nascimento <felipens.dev@gmail.com>
2022-03-10 22:22:39 -0300

    Implementação do menu

---
+++ main.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('#header nav');
+const toogle = document.querySelectorAll('nav .toogle');
+
+for (const element of toogle) {
+  element.addEventListener('click', ()=>{
+    nav.classList.toggle('show');
+  });
+}
