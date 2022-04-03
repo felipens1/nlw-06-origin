e85bbc7041eb6be49b3112f6f8308bb1d16a34d6
Felipe Nascimento <felipens.dev@gmail.com>
2022-04-03 13:05:58 -0300

    Ajustes finais no projeto.

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ for (const link of links) {
 const header = document.querySelector('#header');
 const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', ()=>{
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
   } else {
     header.classList.remove('scroll');
   }
-});
+}
 
 /* Testimonials carousel slider Swiper */
 
@@ -34,7 +34,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mouseWheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 });
 
 /* ScrollReveal: mostrar elementos quando der scroll na página */
@@ -60,10 +66,46 @@ scrollReveal.reveal(
 
 const backToTopButton = document.querySelector('.back-to-top');
 
-window.addEventListener('scroll', () => {
+function backToTop() {
+
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show');
   } else {
     backToTopButton.classList.remove('show');
   }
+
+}
+
+/* Ativar menu conforme scroll da página */
+
+const sections = document.querySelectorAll('main section[id]');
+
+function activeMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (let section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= (sectionTop + sectionHeight);
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll();
+  backToTop();
+  activeMenuAtCurrentSection();
 });
